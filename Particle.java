b9a0d375b8f7089521baeabb12a458c3ad62900a
Kamran Nag <kamrann2353@gmail.com>
2024-08-22 21:15:53 -0500

    particles stack at rest
    need to fix particles sticking

--- Particle.java
+++ Particle.java
@@ -83,8 +83,8 @@ public class Particle {
             double v_p1_parallel = this.velocity.dot(distance_vector);
             double v_p2_parallel = p2.velocity.dot(distance_vector);
 
-            double v_p1_parallel_post = (v_p1_parallel * (this.mass - p2.mass) + 2 * p2.mass * v_p2_parallel) / (this.mass + p2.mass);
-            double v_p2_parallel_post = (v_p2_parallel * (p2.mass - this.mass) + 2 * this.mass * v_p1_parallel) / (this.mass + p2.mass);
+            double v_p1_parallel_post = (overlap / min_distance) * (v_p1_parallel * (this.mass - p2.mass) + 2 * p2.mass * v_p2_parallel) / (this.mass + p2.mass);
+            double v_p2_parallel_post = (overlap / min_distance) * (v_p2_parallel * (p2.mass - this.mass) + 2 * this.mass * v_p1_parallel) / (this.mass + p2.mass);
             
             this.velocity = this.velocity.add(distance_vector.multiply(v_p1_parallel_post - v_p1_parallel));
             p2.velocity = p2.velocity.add(distance_vector.multiply(v_p2_parallel_post - v_p2_parallel));
